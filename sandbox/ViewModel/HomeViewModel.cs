0ba885d7458e1d63003b9eee5a7cc6c5ae1a629d
Praveencls <praveencls@yahoo.com>
2014-10-18 23:33:03 +0530

    ViewModel

---
+++ sandbox/ViewModel/HomeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using CoreLib.Models.ItemTypes;
+using Glass.Mapper.Sc;
+using Sitecore.Mvc.Helpers;
+using Sitecore.Mvc.Presentation;
+
+namespace website.ViewModel
+{
+    public class HomeViewModel : IRenderingModel
+    {
+        private ISitecoreContext _context = new SitecoreContext();
+
+        private SitecoreHelper helper;
+
+        public HtmlString BannerText
+        {
+            get
+            {
+                return helper.Field("Banner Text");
+            }
+        }
+
+        #region IRenderingModel Members
+
+        public void Initialize(Rendering rendering)
+        {
+            SiteRoot = _context.CreateType<SiteRoot>(rendering.Item);
+            //helper = Sitecore.Mvc.Helpers.
+        }
+
+        #endregion IRenderingModel Members
+
+        public string MyHello { get { return "Hello World"; } }
+
+        public SiteRoot SiteRoot { get; set; }
+    }
+}
